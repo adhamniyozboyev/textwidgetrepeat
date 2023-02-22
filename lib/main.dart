1368b4703e5d12beb0ded0de789456321d71905a
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-22 15:59:32 +0500

    pauze

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    body: Center(
+      child: Text(
+        'hello',
+        style: TextStyle(
+          fontSize: 78,
+            decoration: TextDecoration.none,
+            fontStyle: FontStyle.italic,
+            color: Colors.red,
+            letterSpacing: 40),
+        textAlign: TextAlign.center,
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  )));
 }
